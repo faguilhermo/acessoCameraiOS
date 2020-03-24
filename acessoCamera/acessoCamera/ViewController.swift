c814d92c6721ac9cd78dcd1e9bc1a0f6a48af979
Fabrício <guilhermo.fabricio@gmail.com>
2020-03-24 14:39:09 -0300

    Initial commit

---
+++ acessoCamera/acessoCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  acessoCamera
+//
+//  Created by Fabrício Guilhermo on 24/03/20.
+//  Copyright © 2020 Fabrício Guilhermo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
